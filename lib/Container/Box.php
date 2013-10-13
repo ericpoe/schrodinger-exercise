a8f0fb1fcf5764e4a5e6a18dfdfd514132ecb160
ericpoe <eric.poe@gmail.com>
2013-10-13 05:16:10 +0000

    First, basic box setup

---
+++ lib/Container/Box.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: eric
+ * Date: 10/12/13
+ * Time: 3:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Schrodinger\Container;
+
+
+class Box {}

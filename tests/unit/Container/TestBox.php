79a0b1959c0896a290188611d14df11f6add4d54
ericpoe <eric.poe@gmail.com>
2013-10-13 05:13:41 +0000

    Create initial Box test

---
+++ tests/unit/Container/TestBox.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: eric
+ * Date: 10/11/13
+ */
+
+use Schrodinger\Container\Box;
+
+class TestBox extends \PHPUnit_Framework_TestCase
+{
+    function testCreateBox()
+    {
+        $box = new Box();
+    }
+}

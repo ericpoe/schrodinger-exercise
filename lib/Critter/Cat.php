02fb58e1357dc1985c6e88a95189662c61d7d405
ericpoe <eric.poe@gmail.com>
2013-10-13 05:17:40 +0000

    Initial Cat class

---
+++ lib/Critter/Cat.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: eric
+ * Date: 10/12/13
+ * Time: 9:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Schrodinger\Critter;
+
+use Schrodinger\Critter;
+
+class Cat implements Critter {
+    public function isAlive(){}
+
+    public function getMood(){}
+
+    public function kill(){}
+
+    public function speak(){}
+}

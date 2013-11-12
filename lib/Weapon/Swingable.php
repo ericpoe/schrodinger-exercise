947bc30e3a48d5ca23ca9725a47a080b9423091d
ericpoe <eric.poe@gmail.com>
2013-11-11 22:55:18 -0600

    Created initial Swingable interface. I'm just playing with the architecture at this point.

---
+++ lib/Weapon/Swingable.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Swingable.
+ * When a weapon is swingable, it is used by swinging at the end of a lever of some kind with one or more fulcrums
+ * (fulcra).
+ * @package Schrodinger
+ */
+namespace Schrodinger\Weapon;
+
+interface Swingable
+{
+    public function attack(); // information about the swing
+}

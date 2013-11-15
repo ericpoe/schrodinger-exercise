a3553306a9b42564ad6003611b74ebbebbd0e5a9
ericpoe <eric.poe@gmail.com>
2013-11-14 21:25:18 -0600

    Init BoxTest

--- tests/unit/Container/BoxTest.php
+++ tests/unit/Container/BoxTest.php
@@ -9,7 +9,19 @@
 namespace SchrodingerTest;
 
 
-class BoxTest extends \PHPUnit_Framework_TestCase {
+class BoxTest extends \PHPUnit_Framework_TestCase
+{
+    protected $box;
+
+    public function setUp()
+    {
+        $this->$box = new Box();
+    }
+
+    public function boxIsObservable()
+    {
+
+    }
 
 }
  

3655bab75fe63d7d19dfde824f1d251fa8df9126
ericpoe <eric.poe@gmail.com>
2013-11-16 17:36:53 +0000

    Built a basic Box with functional isObservable method

---
+++ lib/Container/Box.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Box.
+ * A Box is the most basic form of a Schrodinger Container.
+ * @package Schrodinger
+ */
+
+namespace Schrodinger\Container;
+
+
+use Schrodinger\SchrodingerI;
+
+class Box implements SchrodingerI
+{
+    protected $boxIsObservable;
+
+    public function __construct()
+    {
+        $this->boxIsObservable = true;
+    }
+
+    public function isObservable()
+    {
+        return $this->boxIsObservable;
+    }
+
+    public function containsCritter()
+    {
+
+    }
+
+    public function containsWeapon()
+    {
+
+    }
+}
